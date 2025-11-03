aa1a6b8eed831f0b274dd3c3f8307bdab7e0e810
Guilherme Canella <guycanella@gmail.com>
2025-11-03 16:18:33 -0300

    implement derived types

---
+++ src/types/lsda_types.f90
@@ -0,0 +1,13 @@
+module lsda_types
+    use lsda_constants, only: dp
+    implicit none
+
+    type :: system_params_t
+        integer :: Na
+        integer :: Nup
+        integer :: Ndown
+        integer :: bc
+        real(dp) :: U
+        real(dp) :: phase
+    end type system_params_t
+end module lsda_types
