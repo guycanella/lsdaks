4b69ae7b11dc6274f136bae6947d0c4069723913
Guilherme Canella <guycanella@gmail.com>
2025-11-07 09:00:31 -0300

    implement tests for line_search function

--- test/test_nonlinear_solvers.f90
+++ test/test_nonlinear_solvers.f90
@@ -13,7 +13,9 @@ contains
         tests = test_list([ &
             test("solve_2x2", test_solve_2x2), &
             test("solve_identity", test_solve_identity), &
-            test("solve_inputs_not_modified", test_inputs_not_modified) &
+            test("solve_inputs_not_modified", test_inputs_not_modified), &
+            test("line_search_full_step", test_line_search_full_step), &
+            test("line_search_positive_alpha", test_line_search_positive_alpha) &
         ])
     end function
 
@@ -55,7 +57,7 @@ contains
         use fortuno_serial, only: check => serial_check
         use nonlinear_solvers, only: solve_linear_system
         use lsda_constants, only: dp
-        
+
         real(dp) :: A(2,2), A_original(2,2)
         real(dp) :: b(2), b_original(2)
         real(dp) :: x(2)
@@ -71,6 +73,74 @@ contains
         ! Check that A and b have not changed
         call check(all(abs(A - A_original) < 1.0e-14_dp))
         call check(all(abs(b - b_original) < 1.0e-14_dp))
+    end subroutine
+
+    subroutine test_line_search_full_step()
+        use fortuno_serial, only: check => serial_check
+        use nonlinear_solvers, only: line_search
+        use bethe_equations, only: initialize_quantum_numbers, compute_residual
+        use lsda_constants, only: dp, TWOPI
+        
+        integer :: Nup, M, L
+        real(dp) :: U, alpha
+        real(dp), allocatable :: x(:), dx(:), F_old(:), I(:), J(:)
+        
+        ! Small system
+        Nup = 2
+        M = 1
+        L = 10
+        U = 4.0_dp
+        
+        allocate(x(Nup+M), dx(Nup+M), I(Nup), J(M))
+        
+        call initialize_quantum_numbers(Nup, M, I, J)
+        
+        ! Initial position
+        x = [0.1_dp, -0.1_dp, 0.05_dp]
+
+        ! Direction that reduces the residual (simulated)
+        dx = -0.01_dp * x  ! Small step in the opposite direction
+
+        F_old = compute_residual(x(1:Nup), x(Nup+1:), I, J, L, U)
+        alpha = line_search(x, dx, F_old, I, J, L, U)
+
+        ! For small and good direction, should accept full step
+        call check(alpha > 0.5_dp)  ! Alpha should be reasonably large
+
+        deallocate(x, dx, I, J)
+    end subroutine
+
+    subroutine test_line_search_positive_alpha()
+        use fortuno_serial, only: check => serial_check
+        use nonlinear_solvers, only: line_search
+        use bethe_equations, only: initialize_quantum_numbers, compute_residual
+        use lsda_constants, only: dp
+        
+        integer :: Nup, M, L
+        real(dp) :: U, alpha
+        real(dp), allocatable :: x(:), dx(:), F_old(:), I(:), J(:)
+        
+        Nup = 2
+        M = 1
+        L = 10
+        U = 4.0_dp
+        
+        allocate(x(Nup+M), dx(Nup+M), I(Nup), J(M))
+        
+        call initialize_quantum_numbers(Nup, M, I, J)
+        
+        x = [0.5_dp, -0.3_dp, 0.2_dp]
+        dx = [0.1_dp, 0.1_dp, 0.1_dp]
+        
+        F_old = compute_residual(x(1:Nup), x(Nup+1:), I, J, L, U)
+        
+        alpha = line_search(x, dx, F_old, I, J, L, U)
+        
+        ! Alpha should be always positive and less than or equal to 1
+        call check(alpha > 0.0_dp)
+        call check(alpha <= 1.0_dp)
+        
+        deallocate(x, dx, I, J)
     end subroutine
 
 end program
