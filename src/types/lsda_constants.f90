9176482ef5448031a01c672b8a1ae016937ee92a
Guilherme Canella <guycanella@gmail.com>
2025-11-07 11:42:50 -0300

    add constants for Newton-Raphson method

--- src/types/lsda_constants.f90
+++ src/types/lsda_constants.f90
@@ -10,9 +10,12 @@ module lsda_constants
     real(dp), parameter :: PI = 4.0_dp * atan(1.0_dp)   ! π = 3.14159...
     real(dp), parameter :: TWOPI = 2.0_dp * PI          ! 2π = 6.28318...
 
-    real(dp), parameter :: U_SMALL = 1.0e-9_dp            ! Check if U ≈ 0
+    real(dp), parameter :: U_SMALL = 1.0e-9_dp          ! Check if U ≈ 0
     real(dp), parameter :: TOL_DEFAULT = 1.0e-16_dp     ! Convergence tol
 
     integer, parameter :: ITER_MAX = 10000              ! Max iterations SCF
     real(dp), parameter :: INITIAL_MIX = 0.95_dp        ! Initial mixing factor
+
+    real(dp), parameter :: NEWTON_TOL = 1.0e-10_dp      ! Newton convergence tol
+    integer, parameter :: NEWTON_MAX_ITER = 50         ! Max Newton iterations
 end module lsda_constants
